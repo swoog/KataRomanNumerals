b1a4f6e8ebe854ccff8db87b69a8ac19286ea6ed
Aurélien GALTIER <aurelien.galtier@live.fr>
2016-01-07 14:59:15 +0100

    Add Should_return_I_when_translate_1_to_roman_number

---
+++ KataRomanNumerals/RomanNumeralsTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataRomanNumerals
+{
+    using Xunit;
+
+    public class RomanNumeralsTests
+    {
+        [Fact]
+        public void Should_return_I_when_translate_1_to_roman_number()
+        {
+            // Arrange
+            var romanNumerals = new RomanNumerals();
+            
+            // Act
+            var romanNumber = romanNumerals.Translate(1);
+
+            // Assert
+            Assert.Equal("I", romanNumber);
+        }
+    }
+
+    public class RomanNumerals
+    {
+        public string Translate(int romanNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
